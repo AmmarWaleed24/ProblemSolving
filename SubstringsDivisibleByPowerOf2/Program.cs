74f510b1913ae33bc6b706988e4893ea532dc7d1
AmmarWaleed <ongo9972@gmail.com>
2024-10-20 21:56:41 +0300

    commit solution file

---
+++ SubstringsDivisibleByPowerOf2/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+class Program
+{
+    public static int[] CountSubstr(string s, int p)
+    {
+        List<int> mylist = new();
+        long divider = (long)Math.Pow(2, p);
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            int counter = 0;
+            long number = 0;
+
+            for (int j = i; j < s.Length; j++)
+            {
+                number = number * 10 + (s[j] - '0'); 
+
+                if (number % divider == 0) counter++;
+            }
+
+            mylist.Add(counter);
+        }
+
+        return mylist.ToArray(); 
+    }
+    static void Main(string[] args)
+    {
+        
+    }
+
+}
