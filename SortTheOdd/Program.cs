a3f4a094d0df1bed2d024bb1f2b4475258b0f640
AmmarWaleed <ongo9972@gmail.com>
2024-10-23 20:12:04 +0300

    commit problem file

---
+++ SortTheOdd/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] arr = { 5, 8, 6, 3, 4 };
+        //List<int> mtx = new List<int>();
+        List<int> ordderedNums = arr.Where(n => n % 2 != 0)  // Keep only odd numbers
+                                     .OrderBy(n => n)         // Sort in ascending order
+                                     .ToList();
+        foreach (int item in ordderedNums)
+            Console.Write($"Odd Numbers Are: {item} ");
+        int r = 0;
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] % 2 != 0)
+            {
+                arr[i] = ordderedNums[r];
+                r++;
+
+            }
+        }
+        Console.WriteLine();
+        foreach (var item in arr)
+        {
+            Console.Write($"{item} ");
+        }
+
+    }
+
+    public static int[] SortArray(int[] array)
+    {
+
+        return array;
+    }
+
+
+}
