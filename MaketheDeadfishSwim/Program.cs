d884eef1e27fbb149e51f1ea155c0b5c65f0f28a
AmmarWaleed <ongo9972@gmail.com>
2024-10-23 15:56:17 +0300

    commit solution dir

---
+++ MaketheDeadfishSwim/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSlearn;
+internal class Program
+{
+
+    private static void Main(string[] arg)
+    {
+
+     
+    }
+
+    public static int[] Parse(string data)
+    {
+        List<int> mylist = new();
+        int counter = 0;
+
+        foreach (char ch in data)
+        {
+            switch (ch)
+            {
+                case 'i':
+                    counter += 1;
+                    break;
+                case 's':
+                    counter *= counter;
+                    break;
+                case 'd':
+                    counter -= 1;
+                    break;
+                case 'o':
+                    mylist.Add(counter);
+                    break;
+            }
+        }
+
+        return mylist.ToArray();
+    }
+ 
+
+}
+
+
+
+
