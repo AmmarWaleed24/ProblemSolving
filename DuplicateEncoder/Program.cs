5856ec8694cc1efa1a9fd1829bf353c23ef36ce6
AmmarWaleed <ongo9972@gmail.com>
2024-10-25 08:14:42 +0300

    commit solution file

---
+++ DuplicateEncoder/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+public class Program
+{
+
+    public static void Main(string[] args)
+
+    {
+
+        DuplicateEncode("recede");
+
+    }
+    //-----
+    public static void DuplicateEncode(string word)
+    {
+        // recede 
+        Dictionary<char, int> freq = new();
+        foreach (char c in word)
+        {
+            if (!freq.ContainsKey(c))
+            {
+                freq[c] = 1;
+            }
+            else
+            {
+                freq[c] += 1; // Increment the count for subsequent occurrences
+            }
+        }
+        foreach (var item in freq)
+        {
+            Console.Write($"{item.Key}: ");
+            Console.WriteLine($"{item.Value}");
+        }
+        //return word;
+    }
+   
+}
