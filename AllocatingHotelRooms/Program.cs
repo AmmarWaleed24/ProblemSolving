5660b4d9d435572e84968034d712c0a39d732338
Ammar Waleed <ongo9972@gmail.com>
2024-10-29 17:41:53 +0300

    Update Program.cs

--- AllocatingHotelRooms/Program.cs
+++ AllocatingHotelRooms/Program.cs
@@ -1,49 +1,67 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
-public class Program
+public class HotelRoomAllocator
 {
-    public static void Main(string[] args)
-    {
-       
-
-    }
-
     public static int[] AllocateRooms(int[][] customers)
     {
-        List<int> Results = new();
-        List<int> RoomAvailabiltyDay = new();
-        //Logic
-        for (int i = 0; i < customers.Length; i++)
+        // Sorting customers by arrival day to allocate rooms in order
+        var sortedCustomers = customers.Select((c, index) => new { arrival = c[0], departure = c[1], index })
+                                       .OrderBy(c => c.arrival)
+                                       .ToArray();
+
+        // Lists to store the result and room availability
+        List<int> results = new();
+        List<int> roomAvailabilityDay = new();
+        
+        foreach (var customer in sortedCustomers)
         {
-            if (RoomAvailabiltyDay.Count == 0)
-            {
-                Results.Add(1);
-                RoomAvailabiltyDay.Add(customers[i][1]);
+            int arrival = customer.arrival;
+            int departure = customer.departure;
+            int customerIndex = customer.index;
 
+            bool roomFound = false;
+            
+            // Check if there's an available room
+            for (int j = 0; j < roomAvailabilityDay.Count; j++)
+            {
+                if (arrival > roomAvailabilityDay[j]) // Room is available
+                {
+                    results.Add(j + 1); // Allocate existing room
+                    roomAvailabilityDay[j] = departure; // Update the room's new availability day
+                    roomFound = true;
+                    break;
+                }
             }
-            else
+            
+            // If no room was available, allocate a new room
+            if (!roomFound)
             {
-                bool RoomFound = false;
-                for (int j = 0; j < RoomAvailabiltyDay.Count; j++)
-                {
-                    if (customers[i][0] > RoomAvailabiltyDay[j])
-                    {
-                        Results.Add(j + 1);
-                        RoomAvailabiltyDay[j] = customers[i][1];
-                        RoomFound = true;
-                        break;
-                    }
-
-                }
-                //No available room
-                if (RoomFound == false)
-                {
-                    Results.Add(RoomAvailabiltyDay.Count + 1);
-                    RoomAvailabiltyDay.Add(customers[i][1]);
-                }
+                results.Add(roomAvailabilityDay.Count + 1); // New room number
+                roomAvailabilityDay.Add(departure); // Add its availability day
             }
         }
-        return Results.ToArray();
+
+        // Rearrange results to original customer order
+        int[] orderedResults = new int[customers.Length];
+        foreach (var customer in sortedCustomers)
+        {
+            orderedResults[customer.index] = results[sortedCustomers.ToList().IndexOf(customer)];
+        }
+
+        return orderedResults;
+    }
+
+    public static void Main()
+    {
+        int[][] customers = new int[][] { new int[] { 1, 5 }, new int[] { 2, 4 }, new int[] { 6, 8 }, new int[] { 7, 7 } };
+        int[] roomAssignments = AllocateRooms(customers);
+
+        Console.WriteLine("Room Assignments:");
+        foreach (int room in roomAssignments)
+        {
+            Console.WriteLine(room);
+        }
     }
 }
